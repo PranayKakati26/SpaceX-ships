4f8a50e50a39c3d8cf7353239d150085f3eba481
PranayKakati26 <pranaykakati35@gmail.com>
2021-09-23 18:08:33 +0530

    Updates

---
+++ precache-manifest.e295ba4f751351bff51f0389a7995a28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31fa0629ba408a340c54357ca25040c2",
+    "url": "/SpaceX-ships/index.html"
+  },
+  {
+    "revision": "b310b5378e5bf564fc43",
+    "url": "/SpaceX-ships/static/css/main.203b70e6.chunk.css"
+  },
+  {
+    "revision": "b4d4515d239e1f283ee5",
+    "url": "/SpaceX-ships/static/js/2.b4c1d1fa.chunk.js"
+  },
+  {
+    "revision": "b310b5378e5bf564fc43",
+    "url": "/SpaceX-ships/static/js/main.8f397deb.chunk.js"
+  },
+  {
+    "revision": "66750663772078d5212c",
+    "url": "/SpaceX-ships/static/js/runtime~main.1640f827.js"
+  }
+]);
